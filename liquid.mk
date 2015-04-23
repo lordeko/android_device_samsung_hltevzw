5f7da07f2263f221b64f396da771fb4cc562acb2
lordeko <mattia.ruggeri88@gmail.com>
2015-04-24 00:32:17 +0200

    Update and rename du.mk to liquid.mk

---
+++ liquid.mk
@@ -0,0 +1,18 @@
+# Boot animation
+TARGET_SCREEN_HEIGHT := 1920
+TARGET_SCREEN_WIDTH := 1080
+
+# Inherit device conf
+$(call inherit-product, device/samsung/hlte/full_hltevzw.mk)
+
+# Enhanced NFC
+$(call inherit-product, vendor/liquid/config/nfc_enhanced.mk)
+
+# Inherit some common Liquid stuff.
+$(call inherit-product, vendor/liquid/config/common_phone.mk)
+
+PRODUCT_DEVICE := hltevzw
+PRODUCT_NAME := liquid_hltevzw
+PRODUCT_BRAND := samsung
+PRODUCT_MODEL := Note 3
+PRODUCT_MANUFACTURER := samsung
