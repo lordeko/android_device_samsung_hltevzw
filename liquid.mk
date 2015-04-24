f9e7430ca386cc97c1527bcea1d6315f666afb56
lordeko <mattia.ruggeri88@gmail.com>
2015-04-24 09:52:50 +0200

    Update liquid.mk

--- liquid.mk
+++ liquid.mk
@@ -3,7 +3,7 @@ TARGET_SCREEN_HEIGHT := 1920
 TARGET_SCREEN_WIDTH := 1080
 
 # Inherit device conf
-$(call inherit-product, device/samsung/hlte/full_hltevzw.mk)
+$(call inherit-product, device/samsung/hltevzw/full_hltevzw.mk)
 
 # Enhanced NFC
 $(call inherit-product, vendor/liquid/config/nfc_enhanced.mk)
